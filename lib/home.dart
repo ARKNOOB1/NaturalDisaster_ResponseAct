34e5447f0cd5758b13247c2632e9f20f8f9fc920
SMART24\User <fds44fs@nate.com>
2025-06-02 14:20:16 +0900

    second_commit

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("자연재해대응"),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, "/rep-disaster"),
+                child: const Text("대표재해")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/rep_disaster.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RepDisaster extends StatefulWidget {
+  const RepDisaster({super.key});
+
+  @override
+  State<RepDisaster> createState() => _RepDisasterState();
+}
+
+class _RepDisasterState extends State<RepDisaster> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
